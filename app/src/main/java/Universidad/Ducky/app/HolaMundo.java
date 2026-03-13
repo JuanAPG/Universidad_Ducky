a3179c9de80c23c6d5c98bfb59f40f969d7e4744
JuanGalvan <jagnt545@gmail.com>
2026-03-13 16:45:04 -0600

    Hola Mundo

---
+++ app/src/main/java/Universidad/Ducky/app/HolaMundo.java
@@ -0,0 +1,16 @@
+package Universidad.Ducky.app;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HolaMundo {
+    @GetMapping("/")
+    public String hola() {
+        return "Hola Mundo!";
+    }
+    
+
+}
